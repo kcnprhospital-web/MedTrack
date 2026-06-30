2c4fb34a7b29ba751b30bc014182d71e185f8a9f
kcnprhospital-web <kcnprhospital@gmail.com>
2026-06-30 17:40:32 +0700

    Add files via upload

---
+++ test-sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('push', function(e) {});
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(self.clients.claim()); });
